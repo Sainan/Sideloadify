2fb0780561a77b947277b93dc160ab6a0674cb76
Sainan <63328889+Sainan@users.noreply.github.com>
2025-05-22 07:25:35 +0200

    Add CLI build

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <fstream>
+#ifdef CLI_BUILD
+#include <iostream>
+#endif
 #include <windows.h>
 
 #include <main.hpp>
@@ -9,7 +12,11 @@ int entrypoint(std::vector<std::string>&& args, bool console)
 {
 	if (args.size() < 2)
 	{
+#ifdef CLI_BUILD
+		std::cout << "Syntax: sideloadify <exe path>" << std::endl;
+#else
 		MessageBoxA(0, "Drop the EXE you would like to sideloadify onto sideloadify.exe.", "Sideloadify", MB_OK | MB_ICONERROR);
+#endif
 		return 1;
 	}
 
@@ -26,21 +33,36 @@ int entrypoint(std::vector<std::string>&& args, bool console)
 			exe.file.write("\0\0", 2);
 
 			path.append(" has successfully been sideloadified.");
+#ifndef CLI_BUILD
 			MessageBoxA(0, path.c_str(), "Sideloadify", MB_OK | MB_ICONINFORMATION);
+#endif
 		}
 		else
 		{
 			path.append(" seems to already be sideloadified.");
+#ifndef CLI_BUILD
 			MessageBoxA(0, path.c_str(), "Sideloadify", MB_OK | MB_ICONINFORMATION);
+#endif
 		}
+#ifdef CLI_BUILD
+		std::cout << path << std::endl;
+#endif
 	}
 	catch (const std::exception& e)
 	{
+#ifdef CLI_BUILD
+		std::cout << e.what() << std::endl;
+#else
 		MessageBoxA(0, e.what(), "Sideloadify", MB_OK | MB_ICONERROR);
+#endif
 		return 1;
 	}
 
 	return 0;
 }
 
+#ifdef CLI_BUILD
+SOUP_MAIN_CLI(entrypoint)
+#else
 SOUP_MAIN_GUI(entrypoint)
+#endif
